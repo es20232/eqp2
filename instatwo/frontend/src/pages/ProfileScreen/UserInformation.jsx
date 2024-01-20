9b330500136af59f574b6039597a0f1effa0b14a
claudiney63 <claudineyryan75@gmail.com>
2024-01-20 03:35:57 -0300

    add information profile screen

--- instatwo/frontend/src/pages/ProfileScreen/UserInformation.jsx
+++ instatwo/frontend/src/pages/ProfileScreen/UserInformation.jsx
@@ -1,20 +1,40 @@
-import React from "react";
-import {Row, Col, Button } from "react-bootstrap";
+import { React, useState } from "react";
+import { Row, Col, Container, Button } from "react-bootstrap";
 import ProfileImage from "../../components/ProfileImage";
+import ButtonComponent from "../../components/ButtonComponent";
+
+const LimitedText = ({ text, limit }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleExpanded = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  return (
+    <div>
+      <p className={isExpanded ? "" : "text-truncate"}>
+        {isExpanded ? text : `${text.slice(0, limit)}...`}
+      </p>
+      {isExpanded ? (
+        <Button variant="link" onClick={toggleExpanded} className="btn-sm">
+          ler menos...
+        </Button>
+      ) : (
+        <Button variant="link" onClick={toggleExpanded} className="btn-sm">
+          ler mais...
+        </Button>
+      )}
+    </div>
+  );
+};
 
 function UserInformation() {
+  const loremIpsum =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+
   return (
-    <div
-      style={{
-        width: "60%",
-        height: "200px",
-        marginLeft: "20%",
-        marginRight: "20%",
-        padding: "20px",
-      }}
-      className="border-bottom"
-    >
-      <Row>
+    <Container fluid className="d-flex justify-content-center vh-90">
+      <Row className="d-flex ">
         <Col xs="auto" style={{ marginRight: "40px" }}>
           <ProfileImage
             imageURL="https://htmlcolorcodes.com/assets/images/colors/bright-blue-color-solid-background-1920x1080.png"
@@ -22,25 +42,28 @@ function UserInformation() {
             size="150px"
           />
         </Col>
-        <Col xs="auto">
+        <Col xs="auto" style={{ maxWidth: "400px", maxHeight: "100px" }}>
+          <h1>Nome</h1>
+          <p style={{ color: "#cccccc" }}>@Nick</p>
+          <LimitedText text={loremIpsum} limit={100} />
+        </Col>
+
+        <Col>
           <Row>
-            <Col>
-              <h5>Nome</h5>
-              <h5>Nome do Usuário</h5>
-              <p>Bio do User</p>
+            <Col className="d-flex flex-column align-items-center justify-content-center">
+              <h1>1243</h1>
+              <p>seguidores</p>
+              <ButtonComponent text="sair" buttonColor="#999999" />
             </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Button variant="secondary">Sair</Button>
-            </Col>
-            <Col>
-              <Button variant="primary">Editar</Button>
+            <Col className="d-flex flex-column align-items-center justify-content-center">
+              <h1>567</h1>
+              <p>seguindo</p>
+              <ButtonComponent text="editar" />
             </Col>
           </Row>
         </Col>
       </Row>
-    </div>
+    </Container>
   );
 }
 
