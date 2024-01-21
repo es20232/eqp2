49aa06248bd1fac7daa8dd587a54df4e0d84b956
claudiney63 <claudineyryan75@gmail.com>
2024-01-21 16:50:01 -0300

    add upload button image in navbar

--- instatwo/frontend/src/components/ButtonComponent.jsx
+++ instatwo/frontend/src/components/ButtonComponent.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import { Button } from "react-bootstrap";
 
 export default function ButtonComponent(props) {
-  const { size, textColor, buttonColor, variant, text, onClick } = props;
+  const { size, textColor, buttonColor, variant, text, onClick, isRound } = props;
 
   const buttonStyle = {
     color: textColor || "white",
     backgroundColor: buttonColor || "#267094",
     borderColor: buttonColor || "transparent",
     width: size || "120px",
-    height: "40px",
+    height: isRound ? size || "40px" : "40px", // Ajuste para tornar o botão redondo
+    borderRadius: isRound ? "50%" : "0", // Ajuste para tornar o botão redondo
     fontWeight: "bold",
     transition: "opacity 0.4s ease", // Adicionando transição de opacidade
   };
--- instatwo/frontend/src/components/NavBarComponent.jsx
+++ instatwo/frontend/src/components/NavBarComponent.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { useState } from "react";
 import { Navbar, Container, Row, Col } from "react-bootstrap";
+import { FaPlus } from "react-icons/fa";
 import ProfileImage from "./ProfileImage"; // Substitua pelo caminho real do seu componente
+import ButtonComponent from "./ButtonComponent"; // Substitua pelo caminho real do seu componente
 
 function NavBarComponent() {
-
   let color_fundo = "#13293d";
   let color_texto = "white";
   let color_hover = "#006494";
@@ -43,28 +44,43 @@ function NavBarComponent() {
           <Navbar.Brand href="#home" style={logoStyle}>
             InstaTwo
           </Navbar.Brand>
-          <Row
-            className="align-items-center no-gutters p-0 m-0"
-            style={rowStyle}
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-          >
-            <Col xs="auto" className="p-0 m-0">
-              <Navbar.Brand
-                href="#home"
-                style={{ color: "white", fontSize: "18px" }}
-              >
-                Meu Perfil
-              </Navbar.Brand>
+
+          <Row>
+            <Col xs="auto" className="d-flex align-items-center">
+            <ButtonComponent
+              textColor="white"
+              text={<FaPlus/>}
+              size="48px"
+              isRound={'true'}
+              buttonColor="#267094"
+            />
             </Col>
-            <Col xs="auto" className="p-0 m-0">
-              <Navbar.Brand>
-                <ProfileImage
-                  imageURL="https://htmlcolorcodes.com/assets/images/colors/bright-blue-color-solid-background-1920x1080.png"
-                  altText="Profile Image"
-                  size="45px" // Tamanho menor
-                />
-              </Navbar.Brand>
+
+            <Col xs="auto">
+              <Row
+                className="align-items-center no-gutters p-0 m-0"
+                style={rowStyle}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+              >
+                <Col xs="auto" className="p-0 m-0">
+                  <Navbar.Brand
+                    href="#home"
+                    style={{ color: "white", fontSize: "18px" }}
+                  >
+                    Meu Perfil
+                  </Navbar.Brand>
+                </Col>
+                <Col xs="auto" className="p-0 m-0">
+                  <Navbar.Brand>
+                    <ProfileImage
+                      imageURL="https://htmlcolorcodes.com/assets/images/colors/bright-blue-color-solid-background-1920x1080.png"
+                      altText="Profile Image"
+                      size="45px" // Tamanho menor
+                    />
+                  </Navbar.Brand>
+                </Col>
+              </Row>
             </Col>
           </Row>
         </Container>
