e995b294881d22b57c88810d74568cd325a308d4
Rodrigo <rodrigoeduardo438@gmail.com>
2024-01-22 15:59:35 -0300

    fix/super_user

--- instatwo/backend/users/models.py
+++ instatwo/backend/users/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from .managers import UserManager
 
 class User(AbstractUser):
-    username = None
+    username = models.CharField(max_length=100, unique=True, null=True)
     password = models.CharField(max_length=255)
     email = models.CharField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+
 # Create your models here.
